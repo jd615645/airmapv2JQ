34945828cd8dd76636a97c616a77c6f677970aea
jd615645 <jd615645@gmail.com>
2017-05-14 23:51:37 +0800

    :cat2:update

--- src/js/main.js
+++ src/js/main.js
@@ -12,16 +12,14 @@ $(document).ready(function () {
   var humiGapColor = ['#fac090', '#76b531', '#b7dee8', '#215968']
 
   // data
-  var marker = {'lass': [], 'lass4u': [], 'lassmaps': [], 'probecube': [], 'indie': [], 'airbox': [], 'epa': []}
-  var airData = {'lass': [], 'lass4u': [], 'lassmaps': [], 'probecube': [], 'indie': [], 'airbox': [], 'epa': []}
+  var allData = []
   var emissionData = []
-  var emissionMarker = []
   var filterType = 'pm25'
   var timeType = 'now'
-  var groupView = {'lass': true, 'lass4u': true, 'lassmaps': true, 'probecube': true, 'indie': true, 'airbox': true, 'epa': true}
   var windLayer = false
-  var emissionSite = false
-  var dateProgress = 0
+
+  var markerLayer = {'lass': [], 'lass4u': [], 'lassmaps': [], 'probecube': [], 'indie': [], 'airbox': [], 'epa': []}
+  var emissionMarkerLayer = []
 
   // init
   var map = L.map('map', {
@@ -29,45 +27,38 @@ $(document).ready(function () {
     zoom: 8,
     zoomControl: false
   })
-  var hideWindLayer = false
   L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map)
 
-  let sidebar = L.control.sidebar('sidebar').addTo(map)
+  var sidebar = L.control.sidebar('sidebar').addTo(map)
   $('#resourceLayer input').bootstrapSwitch()
   sidebar.open('filter')
 
   // chartjsInit
-  let ctx = document.getElementById('myChart').getContext('2d')
-  let myLineChart = new Chart(ctx).Line({
-    labels: ['13:45', '14:45', '15:45', '16:45', '17:45', '18:45', '19:45', '20:45', '21:45', '22:45'],
-    datasets: [
-      {
-        fillColor: 'rgba(220, 220, 220, 0.5)',
-        strokeColor: 'rgba(220, 220, 220, 1)',
-        pointColor: 'rgba(220, 220, 220, 1)',
-        pointStrokeColor: '#fff',
-        pointHighlightFill: '#fff',
-        pointHighlightStroke: 'rgba(220,220,220,1)',
-        data: [20, 34, 20, 18, 66, 31, 58, 17, 18, 21]
+  var ctx = $('#myChart')
+  var myLineChart = Chart.Line(ctx, {
+    data: {
+      labels: ['', '', '', '', '', '', '', '', '', ''],
+      datasets: [
+        {
+          fillColor: 'rgba(220, 220, 220, 0.5)',
+          strokeColor: 'rgba(220, 220, 220, 1)',
+          pointColor: 'rgba(220, 220, 220, 1)',
+          pointStrokeColor: '#fff',
+          pointHighlightFill: '#fff',
+          pointHighlightStroke: 'rgba(220,220,220,1)',
+          data: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        }
+      ]
+    },
+    options: {
+      fill: false,
+      scaleShowLabels: false,
+      legend: {
+        display: false
       }
-    ]
-  }, {
-    scaleShowGridLines: true,
-    scaleGridLineColor: 'rgba(0,0,0,.05)',
-    scaleGridLineWidth: 1,
-    scaleShowHorizontalLines: true,
-    scaleShowVerticalLines: true,
-    bezierCurve: false,
-    bezierCurveTension: 0.4,
-    pointDot: true,
-    pointDotRadius: 4,
-    pointDotStrokeWidth: 1,
-    pointHitDetectionRadius: 20,
-    datasetStroke: true,
-    datasetStrokeWidth: 2,
-    datasetFill: true
+    }
   })
 
   var windy
@@ -131,31 +122,35 @@ $(document).ready(function () {
       .apply($, dataSrc)
       .then((...inputData) => {
         $.each(inputData, (ik, iv) => {
+          let marker = []
           $.each(iv[0], (jk, jv) => {
-            let siteName = jv['SiteName']
+            let uniqueKey = jv['uniqueKey']
             let lat = jv['LatLng']['lat']
             let lng = jv['LatLng']['lng']
             let pm25 = jv['Data']['pm25']
-            let humi = jv['Data']['humi']
-            let temp = jv['Data']['temp']
 
             if (_.isNumber(pm25)) {
               let circleMarker = L.circleMarker(new L.LatLng(lat, lng), {
                 color: markerColor(pm25),
                 opacity: 1,
-                fillOpacity: 0.5
+                fillOpacity: 0.5,
+                airData: jv
               }).bindPopup(infoPopup(jv))
-              marker[sites[ik]].push(circleMarker)
-              airData[sites[ik]].push(jv)
+
+              marker.push(circleMarker)
             }
+            if (!_.has(allData, [uniqueKey])) {
+              _.setWith(allData, [uniqueKey], [], Object)
+            }
+            allData[uniqueKey].push(jv)
           })
-          $.each(marker[sites[ik]], (jk, jv) => {
-            jv.addTo(map)
-          })
+
+          markerLayer[sites[ik]] = L.layerGroup(marker).addTo(map)
         })
 
         $.each(sites, (key, site) => {
-          $('#filterGroup button[value="' + site + '"] .count').html(airData[site].length)
+          let siteNum = Object.keys(markerLayer[site]['_layers']).length
+          $('#filterGroup button[value="' + site + '"] .count').html(siteNum)
         })
       })
   }
@@ -167,38 +162,37 @@ $(document).ready(function () {
       iconSize: [20, 20]
     })
     $.getJSON('../data/emission.json', (data) => {
+      let emissionMarker = []
       $.each(data, (key, val) => {
         let lat = val['latitude'],
           lon = val['longitude'],
           name = val['name']
 
         let data = L.marker(new L.LatLng(lat, lon), {
-          icon: emissionsIcon,
-          opacity: 0
+          icon: emissionsIcon
         }).bindPopup('<p>' + name + '</p>')
         emissionMarker.push(data)
         emissionData.push(val)
       })
-      $.each(emissionMarker, (key, val) => {
-        val.addTo(map)
-      })
+      emissionMarkerLayer = L.layerGroup(emissionMarker)
     })
   }
 
   function toggleFilter (type) {
     filterType = type
-    $.each(marker, (ik, iv) => {
-      $.each(marker[ik], (jk, jv) => {
-        let markerData = airData[ik][jk]['Data']
+    $.each(markerLayer, (ik, iv) => {
+      $.each(markerLayer[ik]['_layers'], (jk, jv) => {
+        let markerData = jv['options']['airData']
+        // console.log(markerData)
         let pm25
         if (timeType == 'now') {
-          pm25 = markerData['pm25']
+          pm25 = markerData['Data']['pm25']
         } else {
-          pm25 = airData[ik][jk]['prediction']
+          pm25 = markerData['prediction']
         }
 
-        let temp = markerData['temp']
-        let humi = markerData['humi']
+        let temp = markerData['Data']['temp']
+        let humi = markerData['Data']['humi']
         switch (type) {
           case 'pm25':
           case 'pm25NASA':
@@ -217,20 +211,11 @@ $(document).ready(function () {
   }
 
   function toggleGroup (site) {
-    groupView[site] = !(groupView[site])
-
-    $.each(marker[site], (key, val) => {
-      // show
-      if (groupView[site]) {
-        // val.setStyle({ zIndexOffset: 4 })
-        val.setStyle({ opacity: 1, fillOpacity: 0.5 })
-      }
-      // hide
-      else {
-        // val.setStyle({ zIndexOffset: -1 })
-        val.setStyle({ opacity: 0, fillOpacity: 0 })
-      }
-    })
+    if (map.hasLayer(markerLayer[site])) {
+      map.removeLayer(markerLayer[site])
+    } else {
+      map.addLayer(markerLayer[site])
+    }
   }
 
   function toggleTime (time) {
@@ -246,16 +231,11 @@ $(document).ready(function () {
   }
 
   function emissionSiteToggle () {
-    $.each(emissionMarker, (key, val) => {
-      // show
-      if (emissionSite) {
-        val.setOpacity(1)
-      }
-      // hide
-      else {
-        val.setOpacity(0)
-      }
-    })
+    if (map.hasLayer(emissionMarkerLayer)) {
+      map.removeLayer(emissionMarkerLayer)
+    } else {
+      map.addLayer(emissionMarkerLayer)
+    }
   }
   function markerColor (num) {
     let color = '#eee'
@@ -301,17 +281,41 @@ $(document).ready(function () {
   function infoPopup (data) {
     let uniqueKey = data['uniqueKey']
     let pm25 = data['Data']['pm25']
+    let temp = data['Data']['temp']
+    let humi = data['Data']['humi']
     let pm25Prediction = data['prediction']
-    let date = moment(data['time']).format('YYYY/MM/DD HH:mm')
-    let time = moment(data['time']).format('HH:mm')
-    let historyData = data['history']
-    historyData.reverse()
-    historyData.push(pm25)
+    let date = moment(data['time']).format('YYYY-MM-DD HH:mm')
 
     // chartjsDraw()
     return (
-      '<p>Device ID: ' + uniqueKey + '</p><p>Time: ' + date + '</p><p>pm2.5: ' + pm25 + 'μg/m<sup>3</sup></p><p>pm2.5 Prediction: ' + pm25Prediction + '</p>'
+      '<h4 class="popup-title">' + uniqueKey + '<small>24.305°N / 120.596°E</small></h4><p class="popup-pm25">PM2.5數值<span>' + pm25 + '</span> μg/m <sup> 3</sup></p><p class="popup-pm25Prediction">空氣品質預測<span>' + pm25Prediction + '</span>μg/m<sup>3</sup></p><p class="popup-temp">溫度<span>' + temp + '</span>°C</p> <p class="popup-humi">溼度<span>' + humi + '</span>%</p> <button class="btn btn-info" id="showChart" value="' + uniqueKey + '"><i class="fa fa-area-chart"></i>點擊觀看圖表</button> <p class="popup-time">最後更新時間: ' + date + '</p>'
     )
+  }
+
+  $(document).on('click', '#showChart', function () {
+    let uniqueKey = $(this).attr('value')
+    console.log(uniqueKey)
+    drawChart(allData[uniqueKey][0])
+  })
+
+  function drawChart (data) {
+    let historyTime = []
+    let historyData = data['history']
+    let date = moment(data['time']).format('YYYY/MM/DD HH:mm')
+    historyData.reverse()
+    historyData.push(data['Data']['pm25'])
+
+    for (var i = 9; i >= 0; i--) {
+      let time = moment(data['time']).subtract(i, 'hours').format('HH:mm')
+      historyTime.push(time)
+    }
+
+    $('#myChart').show()
+
+    myLineChart.data.datasets[0].data = historyData
+    myLineChart.data.labels = historyTime
+
+    myLineChart.update()
   }
 
   function updateChart (time, chartData) {
@@ -321,6 +325,7 @@ $(document).ready(function () {
       timeData.push(time)
     }
   }
+
   function drawWindLayer () {
     $.get('../data/gfs.json', { cache: true }).success(function (result) {
       // Add CanvasLayer to the map
@@ -334,19 +339,19 @@ $(document).ready(function () {
       context = canvasOverlay.canvas().getContext('2d')
 
       map.on('dragstart', function () {
-        if (hideWindLayer) {
+        if (windLayer) {
           clearWind()
           redraw()
         }
       })
       map.on('zoomstart', function () {
-        if (hideWindLayer) {
+        if (windLayer) {
           clearWind()
           redraw()
         }
       })
       map.on('resize', function () {
-        if (hideWindLayer) {
+        if (windLayer) {
           clearWind()
           redraw()
         }
@@ -355,8 +360,8 @@ $(document).ready(function () {
       $('#windLayerSw')
         .bootstrapSwitch()
         .on('switchChange.bootstrapSwitch', (event, state) => {
-          hideWindLayer = !hideWindLayer
-          if (hideWindLayer) {
+          windLayer = !windLayer
+          if (windLayer) {
             redraw()
           }else {
             clearWind()
@@ -369,13 +374,14 @@ $(document).ready(function () {
           window.clearTimeout(timer)
         }
 
-        timer = setTimeout(function () { // showing wind is delayed
+        // showing wind is delayed
+        timer = setTimeout(function () {
           var bounds = map.getBounds()
           var size = map.getSize()
 
           windy.start([[0, 0], [size.x, size.y]], size.x, size.y,
             [[bounds._southWest.lng, bounds._southWest.lat ], [bounds._northEast.lng, bounds._northEast.lat]])
-        }, 750)
+        }, 500)
       }
 
       // clear canvas and stop animation
